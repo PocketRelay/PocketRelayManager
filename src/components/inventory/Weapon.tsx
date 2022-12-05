b0e311dd6630aac49bba0bfc75241d8b0af0bb25
Jacobtread <jacobtread@gmail.com>
2022-12-06 12:35:44 +1300

    Weapon state use effect toggle owned, level input and styling

--- src/components/inventory/Weapon.tsx
+++ src/components/inventory/Weapon.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, ReactNode, useState } from "react";
+import { ChangeEvent, ReactNode, useEffect, useState } from "react";
 import { MAX_WEAPON_LEVEL, Weapon as WeaponModel } from "../../inventory";
 import "./Weapon.scss"
 
@@ -8,15 +8,17 @@ interface Properties {
 }
 
 export default function Weapon({ inventory, weapon }: Properties) {
-    const [level, setLevelImpl] = useState(inventory[weapon.level_index]);
+    const [level, setLevelImpl] = useState(0);
+
+    // Effect for keeping the state up to date with the inventory
+    useEffect(() => {
+        setLevelImpl(inventory[weapon.level_index])
+    }, [inventory, weapon])
+
 
     const isOwned = level > 0;
-
     // Path to the weapon image
     const imageURL: string = `/assets/weapons/${weapon.image}`;
-
-    const actionText: string = isOwned ? "Remove" : "Add";
-
 
     function setLevel(level: number) {
         if (level < 0) level = 0;
@@ -25,42 +27,36 @@ export default function Weapon({ inventory, weapon }: Properties) {
         setLevelImpl(level);
     }
 
-    let contents: ReactNode;
 
-    if (isOwned) {
-        contents = (
-            <>
-                <WeaponLevel
-                    level={level}
-                    setLevel={setLevel}
-                    max={MAX_WEAPON_LEVEL} />
-                <button
-                    className="weapon__action"
-                    title="Removes the weapon form the player inventory"
-                    onClick={() => setLevel(0)}>
-                    Remove
-                </button>
-            </>
-        )
-    } else {
-        contents = (
-            <button
-                className="weapon__action"
-                title="Adds the weapon to the player inventory"
-                onClick={() => setLevel(1)}
-            >
-                Add
-            </button>
-        )
+    function toggleOwned() {
+        if (isOwned) {
+            setLevel(0);
+        } else {
+            setLevel(1);
+        }
     }
 
+    const actionText: string = isOwned ? "Remove" : "Add";
+    const actionTitle: string = isOwned ? "Removes the weapon from the player inventory" : "Adds the weapon to the player inventory"
+
     return (
-        <div className="weapon">
+        <div className="weapon" data-rarity={weapon.rarity} data-owned={isOwned}>
             <h2 className="weapon__name">{weapon.name}</h2>
             <div className="weapon__img-wrapper">
                 <img className="weapon__img" src={imageURL} alt={`${weapon.name} Image`} />
             </div>
-            {contents}
+            <WeaponLevel
+                level={level}
+                setLevel={setLevel}
+                max={MAX_WEAPON_LEVEL}
+                disabled={!isOwned}
+            />
+            <button
+                className="weapon__action"
+                title={actionTitle}
+                onClick={toggleOwned}>
+                {actionText}
+            </button>
         </div>
     )
 }
@@ -69,9 +65,10 @@ interface LevelProperties {
     level: number;
     setLevel(value: number): void;
     max: number;
+    disabled: boolean
 }
 
-function WeaponLevel({ level, setLevel, max }: LevelProperties) {
+function WeaponLevel({ level, setLevel, max, disabled }: LevelProperties) {
 
     function setLevelEvent(event: ChangeEvent<HTMLInputElement>) {
         let value = event.target.value;
@@ -83,16 +80,22 @@ function WeaponLevel({ level, setLevel, max }: LevelProperties) {
     }
 
     return (
-        <div className="level">
+        <div className="level" data-disabled={disabled}>
             <label className="level__input input">
-                <span className="input__name">Level</span>
-                <input className="input__value" type="number" value={level} onChange={setLevelEvent} />
+                <span className="level__input__name input__name">Level</span>
+                <input
+                    disabled={disabled}
+                    className="input__value"
+                    type="number"
+                    value={level}
+                    onChange={setLevelEvent} />
             </label>
             <div className="level__actions">
                 <button
                     className="level__actions__button"
                     onClick={() => setLevel(1)}
                     title="Set the level to the minimum level (1)"
+                    disabled={disabled}
                 >
                     Min
                 </button>
@@ -100,6 +103,7 @@ function WeaponLevel({ level, setLevel, max }: LevelProperties) {
                     className="level__actions__button"
                     onClick={() => setLevel(max)}
                     title="Set the level to the max normal level"
+                    disabled={disabled}
                 >
                     Max
                 </button>
@@ -107,6 +111,7 @@ function WeaponLevel({ level, setLevel, max }: LevelProperties) {
                     className="level__actions__button"
                     onClick={() => setLevel(255)}
                     title="Set the level to the max cheated level"
+                    disabled={disabled}
                 >
                     GOD
                 </button>
