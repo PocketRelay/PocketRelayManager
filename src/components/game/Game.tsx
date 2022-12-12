4bad81ce172c5ea960e5d63a51cf5a640d69627b
Jacobtread <jacobtread@gmail.com>
2022-12-12 14:19:48 +1300

    Added missing assets and added map for looking up games

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -1,5 +1,5 @@
 import { Game as GameModel } from "@api/models"
-import { DIFFICULTY_ATTR, DIFFICULTY_IMAGES, ENEMY_ATTR, ENEMY_IMAGES } from "@data/games";
+import { DIFFICULTY_ATTR, DIFFICULTY_IMAGES, ENEMY_ATTR, ENEMY_IMAGES, getMap, Map, MAP_ATTR } from "@data/games";
 import "./Game.scss"
 
 interface Properties {
@@ -10,15 +10,31 @@ export default function Game({ game }: Properties) {
 
     const difficultyImageName: string = DIFFICULTY_IMAGES[game.attributes[DIFFICULTY_ATTR]];
     const difficultyImage: string = `/assets/mp/${difficultyImageName}`;
-    
+
     const enemyImageName: string = ENEMY_IMAGES[game.attributes[ENEMY_ATTR]];
     const enemyImage: string = `/assets/mp/${enemyImageName}`;
 
     return (
-        <div>
+        <div className="game">
+            {/*  Team */}
+            <GameMap game={game} />
             <img src={enemyImage}></img>
             <img src={difficultyImage}></img>
             {JSON.stringify(game)}
+        </div>
+    )
+}
+
+
+function GameMap({ game }: Properties) {
+    const map: Map = getMap(game.attributes[MAP_ATTR]);
+    const mapImage: string = `/assets/mp/maps/${map.image}.png`;
+
+    return (
+        <div className="game__map">
+            <img className="game__map__img" src={mapImage} alt={`${map.name} Image`} />
+            <span className="game__map__name">{map.name}</span>
+            <span className="game__map__location">{map.location}</span>
         </div>
     )
 }
--- src/data/games.ts
+++ src/data/games.ts
@@ -8,13 +8,140 @@ export const ENEMY_IMAGES: Record<string, string> = {
     "enemy1": "OptEmyCerberus.png",
     "enemy2": "OptEmyGeth.png",
     "enemy3": "OptEmyReapers.png",
-    "enemy4": "OptEmyRandom.png",
+    "enemy4": "OptEmyCollector.png",
 }
 
-
 export const DIFFICULTY_IMAGES: Record<string, string> = {
+    "random": "OptChallgRandom.png",
     "difficulty0": "OptChallgBrnz.png",
     "difficulty1": "OptChallgSilver.png",
     "difficulty2": "OptChallgGold.png",
     "difficulty3": "OptChallgPlatinum.png",
+}
+
+export interface Map {
+    name: string;
+    location: string;
+    image: string;
+}
+
+const UNKNOWN_MAP: Map = {
+    name: "Unknown Location",
+    location: "?",
+    image: "OptMapRandom.png"
+}
+
+export function getMap(name: string): Map {
+    const value: Map | undefined = MAPS[name];
+    if (value == undefined) {
+        return UNKNOWN_MAP;
+    } else {
+        return value;
+    }
+}
+
+const MAPS: Record<string, Map> = {
+    // Random Maps
+    "map0": UNKNOWN_MAP,
+    "random": UNKNOWN_MAP,
+    // Standard Maps
+    "map2": {
+        name: "Firebase Dagger",
+        location: "Ontarom",
+        image: "OptMapDish"
+    },
+    "map3": {
+        name: "Firebase Ghost",
+        location: "Benning",
+        image: "OptMapSlum"
+    },
+    "map4": {
+        name: "Firebase Giant",
+        location: "Tuchanka",
+        image: "OptMapTowr"
+    },
+    "map5": {
+        name: "Firebase Reactor",
+        location: "Cyone",
+        image: "OptMapRctr"
+    },
+    "map7": {
+        name: "Firebase Glacier",
+        location: "Sanctum",
+        image: "OptMapCer"
+    },
+    "map8": {
+        name: "Firebase White",
+        location: "Noveria",
+        image: "OptMapNoveria"
+    },
+    // Resurgence Pack Maps
+    "map9": {
+        name: "Firebase Condor",
+        location: "Paleven",
+        image: "OptMapMoon"
+    },
+    "map10": {
+        name: "Firebase Hydra",
+        location: "Arvuna",
+        image: "OptMapGeth"
+    },
+    // Rebellion Pack Maps
+    "map11": {
+        name: "Firebase Jade",
+        location: "Sur'Kesh",
+        image: "OptMapJade"
+    },
+    "map13": {
+        name: "Firebase Goddess",
+        location: "Thessia",
+        image: "OptMapGoddess"
+    },
+    // Earth Maps   
+    "map14": {
+        name: "Firebase Rio",
+        location: "Earth",
+        image: "OptMapRio"
+    },
+    "map15": {
+        name: "Firebase Vancouver",
+        location: "Earth",
+        image: "OptMapHosp"
+    },
+    "map16": {
+        name: "Firebase London",
+        location: "Earth",
+        image: "OptMapLondon"
+    },
+    // Retaliation Hazard Maps
+    "map17": {
+        name: "☣ Firebase Glacier ☣",
+        location: "Sanctum",
+        image: "OptMapSwarm"
+    },
+    "map18": {
+        name: "☣ Firebase Dagger ☣",
+        location: "Ontarom",
+        image: "OptMapSandstorm"
+    },
+    "map19": {
+        name: "☣ Firebase Reactor ☣",
+        location: "Cyone",
+        image: "OptMapMeltdown"
+    },
+    "map20": {
+        name: "☣ Firebase Ghost ☣",
+        location: "Benning",
+        image: "OptMapAcid"
+    },
+    "map21": {
+        name: "☣ Firebase Giant ☣",
+        location: "Tuchanka",
+        image: "OptMapTower"
+    },
+    "map22": {
+        name: "☣ Firebase White ☣",
+        location: "Noveria",
+        image: "OptMapSnowstorm"
+    }
 }
