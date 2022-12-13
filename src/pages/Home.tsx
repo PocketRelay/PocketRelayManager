dbeae7235f7c258b2030ad031e591ce4c830209b
Jacobtread <jacobtread@gmail.com>
2022-12-13 14:34:54 +1300

    Styled and improved home screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,9 @@
 import { TOKEN_KEY, useAppContext } from "@contexts/AppContext";
 import { Link } from "react-router-dom";
+import "./Home.scss";
 
 export default function Home() {
-    const { setToken } = useAppContext();
+    const { setToken, serverState } = useAppContext();
 
     function logout(): void {
         localStorage.removeItem(TOKEN_KEY);
@@ -10,17 +11,33 @@ export default function Home() {
     }
 
     return (
-        <div>
-            <button onClick={logout} className="button">Logout</button>
-            <Link to="/players" className="home__button button">
-                <img src="" alt="" />
-                <span>Players</span>
-            </Link>
+        <div className="list">
+            <nav className="list__actions">
+                <button onClick={logout} className="button">Logout</button>
+                <Link to="/players" className="home__button button">
+                    <img src="" alt="" />
+                    <span>Players</span>
+                </Link>
 
-            <Link to="/games" className="home__button button">
-                <img src="" alt="" />
-                <span>Games</span>
-            </Link>
+                <Link to="/games" className="home__button button">
+                    <img src="" alt="" />
+                    <span>Games</span>
+                </Link>
+            </nav>
+            <div className="list__contents home">
+                <img className="home__logo" src="/assets/logo.svg"/>
+                <h1 className="home__version">Pocket Relay Server</h1>
+                <div className="home__attrs">
+                <div className="home__attr">
+                    <span className="home__attr__name">Version:</span>
+                    <span className="home__attr__value">{serverState.version}</span>
+                </div>
+                <div className="home__attr">
+                    <span className="home__attr__name">BaseURL:</span>
+                    <span className="home__attr__value">{serverState.baseURL}</span>
+                </div>
+                </div>
+            </div>
         </div>
     )
 }
