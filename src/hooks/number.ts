02a2bccb4e081441cfe4aa237de00f464a1f253d
Jacobtread <jacobtread@gmail.com>
2023-01-15 15:54:13 +1300

    Created number input handler

---
+++ src/hooks/number.ts
@@ -0,0 +1,29 @@
+import { ChangeEvent } from "react"
+
+type NumberInputHandler = (event: ChangeEvent<HTMLInputElement>) => void;
+
+/**
+ * Function which provides event handler functions for
+ * setting a number state based on an input that should
+ * produce a number clamped between a min and max value
+ * 
+ * @param min      The min clamp value
+ * @param max      The max clamp value
+ * @param setState The function for updating the number state
+ * @returns        A function for handling the input change event
+ */
+export function handleNumberInput(
+    min: number,
+    max: number,
+    setState: (value: number) => void,
+): NumberInputHandler {
+    return (event: ChangeEvent<HTMLInputElement>) => {
+        const value: string = event.target.value;
+        let parsed: number = parseInt(value);
+        // Handling for non number values
+        if (Number.isNaN(parsed)) parsed = 0;
+        if (parsed < min) parsed = min;
+        if (parsed > max) parsed = max;
+        setState(parsed);
+    }
+}
