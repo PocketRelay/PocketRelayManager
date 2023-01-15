6125ee4d58677db498f9864e4b36d6915561ba9a
Jacobtread <jacobtread@gmail.com>
2023-01-15 15:37:41 +1300

    Documented and cleaned up collapse component

--- src/components/Collapse.tsx
+++ src/components/Collapse.tsx
@@ -1,22 +1,34 @@
-import { ReactNode, useState } from "react";
+import { PropsWithChildren, useState } from "react";
 import "./Collapse.scss";
 
 interface Properties {
+    // The name to display in the heading section
     name: string;
-    children: ReactNode;
 }
 
-export default function Collapse({ name, children }: Properties) {
+/**
+ * Component for a collapsable section with a header and collapsable
+ * child element 
+ * 
+ * @param name     The name to use in the collapse heading
+ * @param children The children to render inside the open collapse
+ */
+export default function Collapse({ name, children }: PropsWithChildren<Properties>) {
     const [isCollapsed, setCollapsed] = useState(true);
 
-    function toggleCollapse() {
-        setCollapsed(value => !value);
-    }
+    // Action for the button to change the collapsed state when clicked
+    const buttonAction = () => setCollapsed(value => !value);
+    // Text for the button to display based on the collapsed state
+    const buttonText = isCollapsed ? "Expand" : "Collapse";
 
     return (
         <div className="collapse" data-collapsed={isCollapsed}>
             <div className="collapse__header">
-                <button onClick={toggleCollapse} className="collapse__header__button">{isCollapsed ? "Expand" : "Collapse"}</button>
+                <button
+                    onClick={buttonAction}
+                    className="collapse__header__button">
+                    {buttonText}
+                </button>
                 <h1 className="collapse__header__name">
                     {name}
                 </h1>
